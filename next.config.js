8bb271ec2fc208a0c9470416edc414a01d0119de
Sean C Davis <scdavis41@gmail.com>
2023-01-27 14:40:31 -0500

    Use JS config and add sb dev hook into next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-const sourcebit = require('sourcebit');
-const sourcebitConfig = require('./sourcebit.js');
+const sourcebit = require("sourcebit");
+const sourcebitConfig = require("./sourcebit.js");
+const { withStackbit } = require("experimental-next-stackbit");
+
 sourcebit.fetch(sourcebitConfig);
 
-module.exports = {};
+module.exports = withStackbit({});
